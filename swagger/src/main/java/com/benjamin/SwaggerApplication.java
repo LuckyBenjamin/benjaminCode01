d65cf4d61ff23f5777116fb074fbbe6ab059239e
benjamin <1225674627@qq.com>
2019-12-17 14:24:22 +0800

    swagger2测试提交

--- swagger/src/main/java/com/benjamin/SwaggerApplication.java
+++ swagger/src/main/java/com/benjamin/SwaggerApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SwaggerApplication {
 
+
+	//启动Spring Boot程序，访问：http://localhost:8080/swagger-ui.html
+
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerApplication.class, args);
 	}
---
+++ swagger/src/main/java/com/benjamin/po/User.java
@@ -0,0 +1,16 @@
+package com.benjamin.po;
+
+import lombok.Data;
+
+/**
+ * @ClassName User
+ * @Description: TODO
+ * @Author Benjamin
+ * @Date 2019/12/17
+ **/
+@Data
+public class User {
+    private long id;
+    private String name;
+    private Integer age;
+}
