37b64fec4a31e7620add3832e5b7e93a79aa787d
benjamin <1225674627@qq.com>
2019-12-27 10:28:40 +0800

    jjjj

---
+++ mybatis/src/main/java/com/benjamin/models/po/User.java
@@ -0,0 +1,13 @@
+package com.benjamin.models.po;
+
+public class User extends UserKey {
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
